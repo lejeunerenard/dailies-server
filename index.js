1aeecdcb21de588967eba88cf697ccc286ed41dc
Sean Zellmer <sean@lejeunerenard.com>
2019-05-22 14:07:36 -0500

    refact: Standardize not found response

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const fs = require('fs')
 const { EventEmitter } = require('events')
 const bb = require('braille-binary')
 
+function respondNotFound (res) {
+  res.writeHead(404, { 'Content-Type': 'text/plain' })
+  res.write('404 Not Found')
+  res.end()
+}
+
 class DailiesServer extends EventEmitter {
   constructor (db) {
     if (!db) throw Error('DailiesServer : No Database provided')
@@ -20,9 +26,7 @@ class DailiesServer extends EventEmitter {
         this.db.latest().then((daily) => {
           let media = daily.media
           if (!(media && media.length)) {
-            res.writeHead(404, { 'Content-Type': 'text/plain' })
-            res.write('404 Not Found')
-            res.end()
+            respondNotFound(res)
           } else {
             res.writeHead(200, { 'Content-Type': 'application/json' })
             res.write(JSON.stringify(media.length))
@@ -34,9 +38,7 @@ class DailiesServer extends EventEmitter {
         this.db.latest().then((daily) => {
           let media = daily.media
           if (!(media && media.length)) {
-            res.writeHead(404, { 'Content-Type': 'text/plain' })
-            res.write('404 Not Found')
-            res.end()
+            respondNotFound(res)
           } else {
             // Only return the requested version
             fs.createReadStream(this.db.getDailyMedia(daily, mediaIndex))
@@ -50,9 +52,7 @@ class DailiesServer extends EventEmitter {
         this.db.latest().then((daily) => {
           let index = daily.dailyIndex
           if (!index) {
-            res.writeHead(404, { 'Content-Type': 'text/plain' })
-            res.write('404 Not Found')
-            res.end()
+            respondNotFound(res)
           } else {
             res.writeHead(200, { 'Content-Type': 'application/json' })
             res.write(JSON.stringify(bb(index) + '|'))
@@ -60,9 +60,7 @@ class DailiesServer extends EventEmitter {
           }
         })
       } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' })
-        res.write('404 Not Found\n')
-        res.end()
+        respondNotFound(res)
       }
     }).listen(process.env.PORT || 8000)
   }
